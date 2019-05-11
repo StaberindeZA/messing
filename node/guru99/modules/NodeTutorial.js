096e2e647ffea11f30a066896940ad44abfbad58
Reino Muhl <reino.muhl@gmail.com>
2019-05-11 15:37:28 -0400

    add to master

---
+++ node/guru99/modules/NodeTutorial.js
@@ -0,0 +1,11 @@
+var Tutor=require('./Tutorial.js');
+exports.NodeTutorial=function()
+{
+console.log("Node Tutorial")
+function pTutor()
+{
+// var PTutor=Tutor
+// PTutor.tutorial();
+console.log("made it");
+}
+}
