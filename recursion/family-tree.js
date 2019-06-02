0bba3199096ad57ba07cd845cf2cdac92b89236f
avatarvault19 <avatarvault19@gmail.com>
2019-06-02 11:42:12 -0400

    Commited

--- recursion/family-tree.js
+++ recursion/family-tree.js
@@ -18,19 +18,24 @@ class FamilyTree {
     return 1 + this.children.length;
   }
 
-  findMember(member) {
-    for (let i = 0; i < this.children.length; i++) {
-      if (this.children[i].value === member) {
-        return this.children[i];
-      }
+  findMember(name) {
+    if (this.value === name) {
+      return this;
     }
-    return undefined;
+
+    return this.children.find(child => {
+      return child.findMember(name);
+    });
   }
 
+  //This method has been modified to also build the DOM.
+  //Use this method to refresh the DOM.
   log() {
     let str = `<p>${'-'.repeat(this.depth * 2 + 2)} ${this.value}</p>\n`;
 
+    //Find/Create the container for the relevant depth/generation.
     const currentContainer = findContainer(this.depth);
+    //Create new Name node and add to current container.
     currentContainer.appendChild(addFamilyNode(this.value));
     
     //base case
@@ -46,37 +51,6 @@ class FamilyTree {
     return str;
   }
 
-  log2() {
-    let str =  `<p>${'-'.repeat(this.depth * 2 + 2)} ${this.value}</p>\n`;
-    let tempStr = '';
-
-    if(this.children.length === 0 ) {
-      return str;
-    } else {
-      // tempStr = this.children.forEach(child => {
-      //   child.log2();
-      // });
-
-      for(let i=0; i < this.children.length; i++)
-      {
-        tempStr += this.children[i].log2();
-      }
-      console.log(tempStr);
-      str = str + tempStr;
-      return str;
-    }
-    
-  }
-}
-
-function createGenerationContainer(depth) {
-  const famContainer = document.createElement('div');
-  famContainer.className = 'form-container';
-  famContainer.dataset.depth = depth;
-
-  // famContainer.appendChild(addFamilyNode('name'));
-
-  return famContainer;
 }
 
 function addFamilyNode(name) {
@@ -87,25 +61,10 @@ function addFamilyNode(name) {
   return famName;
 }
 
-function buildDisplay(currentNode, depth) {
-
-  const memberContainer = createGenerationContainer(depth);
-  memberContainer.appendChild(addFamilyNode(currentNode.name));
-
-  if(currentNode.children.length === 0) {
-    return memberContainer;
-  } else {
-    currentNode.children.forEach( child => {
-      buildDisplay(child, depth + 1);
-    })
-  }
-
-}
-
 function findContainer(depth) {
   //Check DOM container for famContainer
   //If found return
-  const allFamContainer = container.querySelectorAll('.form-container');
+  const allFamContainer = memberContainer.querySelectorAll('.form-container');
   let foundFamContainer = {};
   let nothing = '';
 
@@ -119,14 +78,116 @@ function findContainer(depth) {
     famContainer.className = 'form-container';
     famContainer.dataset.depth = depth;
 
-    container.appendChild(famContainer);
+    memberContainer.appendChild(famContainer);
 
     return famContainer;
   }
 
 }
 
-const container = document.querySelector('.container');
+const state = {
+  age: '',
+  name: '',
+};
+
+let selectedNode = {
+  age: '',
+  name: 'Pop',
+}
+
+const rootEl = document.getElementById('root');
+
+// Create Name Input
+const nameContainer = document.createElement('div');
+nameContainer.className = 'input-container';
+const nameLabel = document.createElement('label');
+nameLabel.for = 'name-input';
+nameLabel.innerText = 'Name';
+nameLabel.className = 'input-label';
+const nameInput = document.createElement('input');
+nameInput.id = 'name-input';
+nameInput.className = 'form-input';
+nameContainer.appendChild(nameLabel);
+nameContainer.appendChild(nameInput);
+
+// Create Age Input
+const ageContainer = document.createElement('div');
+ageContainer.className = 'input-container';
+const ageLabel = document.createElement('label');
+ageLabel.for = 'age-input';
+ageLabel.innerText = 'Age';
+ageLabel.className = 'input-label';
+const ageInput = document.createElement('input');
+ageInput.className = 'form-input';
+ageInput.id = 'age-input';
+ageContainer.appendChild(ageLabel);
+ageContainer.appendChild(ageInput);
+
+// Error Text
+const errorText = document.createElement('span');
+errorText.className = 'error-text';
+errorText.innerText = 'Age must be a number';
+
+// Create Submission
+const submit = document.createElement('button');
+submit.className = 'form-button';
+submit.innerText = 'Add Member';
+const container = document.createElement('div');
+container.className = 'form-container';
+const memberContainer = document.createElement('div');
+memberContainer.className = 'member-container';
+
+// Events
+// Grab Age from Input Field and Store
+ageInput.onchange = e => {
+  state.age = e.target.value;
+
+  errorText.style.visibility = 'hidden';
+  errorText.style.height = 0;
+};
+
+// Grab Name from Input Field and Store
+nameInput.onchange = e => {
+  state.name = e.target.value;
+};
+
+// On Click Do Something Important
+submit.onclick = () => {
+  const ageAsNum = parseInt(state.age);
+
+  if (!isNaN(ageAsNum)) {
+    console.log(szwajkowskis);
+    const currentNode = szwajkowskis.findMember(selectedNode.name);
+    currentNode.insert(state.name);
+    memberContainer.innerHTML = '';
+    szwajkowskis.log();
+    console.log(`${state.name} is ${state.age}, adding now!`);
+    state.age = '';
+    state.name = '';
+    nameInput.value = state.name;
+    ageInput.value = state.age;
+
+    //Add family members on click.
+    //if(!Object.keys(rootFamily).length) {
+
+  } else {
+    console.error(`Error adding ${state.name} age ${state.age}. ${state.age} is not a number.`);
+    errorText.style.visibility = 'visible';
+    errorText.style.height = 'auto';
+  }
+
+};
+
+// Append to DOM
+container.appendChild(nameContainer);
+container.appendChild(ageContainer);
+container.appendChild(errorText);
+container.appendChild(submit);
+
+rootEl.appendChild(container);
+rootEl.appendChild(memberContainer);
+
+// const container = document.querySelector('.container');
 
 const szwajkowskis = new FamilyTree('Pop');
 
@@ -146,6 +207,8 @@ const amysFamily = szwajkowskis.findMember('Amy');
 
 amysFamily.insert('Henry');
 amysFamily.insert('Vivian');
+
+selectedNode.name = 'Todd';
 
 const log = szwajkowskis.log();
 // console.log(`${log}`);
